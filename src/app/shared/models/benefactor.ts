6b08d71a06c8aa95f8e5c13f690b2a811e54d1f7
b-caby <caby.berenger@gmail.com>
2020-03-25 17:37:02 +0100

    fix: fix build

--- src/app/shared/models/benefactor.ts
+++ src/app/shared/models/benefactor.ts
@@ -6,10 +6,13 @@ export class Benefactor {
     public name: string;
     public firstname: string;
     public adress: string;
+    public adress2: string;
     public postalCode: string;
     public city: string;
     public phone: string;
     public mobile: string;
     public email: string;
+    public misc: string;
+    public verifiedDate: string;
     public gifts: Gift[];
 }
